eaceedfb4d7abdd28293296bdbef6da968578fd5
DarianRushworth <darianerrolrushworth@gmail.com>
2020-06-26 09:46:03 +0200

    Making and receiving data from server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const days = {
+    "today" :"Servers, npm,axios",
+    "yesterday" :"Git, callbacks"
+}
+app.get("/json", (request, response) => {
+    response.send(days)
+})
+
+
+const port = 4000
+
+app.listen(
+    port,
+    () =>{
+        console.log(`Listening on: ${port}`)
+    }
+)
---
+++ yesterday.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+ 
+
+async function getJSON(){
+    try{
+        const url = `http://localhost:4000/json`
+        const response = await axios.get(url)
+        
+        
+        const learned = response.data
+        const whatLearned = learned['yesterday']
+        console.log(whatLearned)
+    } catch (error){
+        console.log(error.message)
+    }
+}
+getJSON()
